b28e220b9b62e049e2e24c7133a4bb56a30b9cd4
GABRIEL GALVÃO <gdszcoimbra@gmail.com>
2024-07-18 17:31:55 -0300

    Buscador de Cep em React Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { IoMdSearch } from "react-icons/io";
+import { useState } from "react";
+import './style.css'
+import api from "./services/api";
 function App() {
+  const [input,setInput] = useState('')
+  const [cep, setCep] = useState('')
+  async function handleSearch(){
+      if(input === ''){
+        alert("preencha algum cep")
+        return;
+      }
+
+      try{
+        const response = await api.get(`${input}/json`);
+        console.log(response.data)
+        setCep(response.data)
+        setInput("")
+      }catch{
+        alert("ops erro ao buscar")
+        setInput("")
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title" >Buscador CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP..." value={input}
+        onChange={(e) => setInput(e.target.value)}>
+        </input>
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <IoMdSearch size={25} color="#fff" ></IoMdSearch>
+
+        </button>
+      </div >
+      {Object.keys(cep).length > 0 &&(
+        <main className="main">
+        <h2>CEP: {cep.cep} </h2>
+        <span>{cep.logradouro}</span>
+        <span>{cep.complemento}</span>
+        <span>Bairro {cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+      </main>
+      )} 
+      
     </div>
   );
 }
